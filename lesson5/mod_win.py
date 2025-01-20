a435ec08401eca8546e48b3ee124f71f5cfd7295
My Name_Nastya <avast1991.07.20@gmail.com>
2025-01-20 10:29:27 +0400

    Внесены изменения в mod_win.py

--- lesson5/mod_win.py
+++ lesson5/mod_win.py
@@ -1,9 +1,14 @@
 from selenium import webdriver
 from selenium.webdriver.firefox.service import Service as FirefoxService
 from webdriver_manager.firefox import GeckoDriverManager
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.common.by import By
 
+# Создание драйвера
 driver = webdriver.Firefox(service=FirefoxService(GeckoDriverManager().install()))
 
+# Переход на нужную страницу
 driver.get('http://the-internet.herokuapp.com/entry_ad')
 
 try:
@@ -16,5 +21,5 @@ try:
     # Шаг 2: Нажимаем на кнопку Close
     close_button.click()
 finally:
-    
+    # Завершение работы драйвера
     driver.quit()
